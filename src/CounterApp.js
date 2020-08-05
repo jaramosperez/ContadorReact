8bf5139c347d001815ee6d9d933e72c1db1b5603
Javier Ramos <jaramosperez85@gmail.com>
2020-08-05 00:52:37 -0400

    Primera Aplicación con React, contador

---
+++ src/CounterApp.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import PropTypes from 'prop-types';
+
+const CounterApp = ( {value} ) => {
+
+    const [ counter, setCounter ] = useState(value);
+
+    const handleAdd = () => setCounter( counter + 1 );
+
+    const  handleReset = () => setCounter(value);
+
+    const handleSubtract = () => setCounter( counter - 1 );
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            {/*<pre>  {JSON.stringify( saludo, null, 3)}</pre>*/}
+            <h2>{ counter }</h2>
+
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset}>Reset</button>
+            <button onClick={ handleSubtract}>-1</button>
+        </>
+    );
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+}
+
+export default CounterApp;
