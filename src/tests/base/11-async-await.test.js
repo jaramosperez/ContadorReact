3b47d160c60215fe6e52dbbc4be0eb8659662a2b
Javier Ramos <jaramosperez85@gmail.com>
2020-08-08 23:48:54 -0400

    Purebas async-await

---
+++ src/tests/base/11-async-await.test.js
@@ -0,0 +1,13 @@
+const { getImagen } = require("../../base/11-async-await")
+
+describe( "Pruebas con async-await y Fetch", () => {
+
+    test( 'Debe retornar el url de la imagen', async() => {
+        
+        const url = await getImagen();
+        //console.log(url);
+        expect( url.includes('https://') ).toBe( true );
+        
+    })
+    
+})
